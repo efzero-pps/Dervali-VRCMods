2a1738b06f3e0f861af3f564c8a806b345b54b29
Dervali-git <68404726+Dervali-git@users.noreply.github.com>
2020-10-28 19:26:45 -0500

    RemoveChairs
    Simple mod that deletes all chairs in a world

---
+++ RemoveChairs/Main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using MelonLoader;
+using UIExpansionKit.API;
+using UnityEngine;
+using VRC.SDKBase;
+
+[assembly: MelonInfo(typeof(RemoveChairs.Main), "RemoveChairs", "1.0.0", "Nirvash")]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+
+namespace RemoveChairs
+{
+    public class Main : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonMod uiExpansionKit = MelonLoader.Main.Mods.Find(m => m.InfoAttribute.Name == "UI Expansion Kit");
+            if (uiExpansionKit != null)
+            {
+                uiExpansionKit.InfoAttribute.SystemType.Assembly.GetTypes().First(t => t.FullName == "UIExpansionKit.API.ExpansionKitApi").GetMethod("RegisterWaitConditionBeforeDecorating", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static).Invoke(null, new object[]
+                {
+                    CreateQuickMenuButton()
+                });
+            }
+        }
+
+        private IEnumerator CreateQuickMenuButton()
+        {
+                while (QuickMenu.prop_QuickMenu_0 == null) yield return null;
+                ExpansionKitApi.RegisterSimpleMenuButton(ExpandedMenu.SettingsMenu, "Remove All Chairs", new Action(() =>
+                {
+                    var objects = Resources.FindObjectsOfTypeAll<VRCStation>();
+                    foreach (var item in objects)
+                    {
+                        UnityEngine.Object.Destroy(item.gameObject); // item.gameObject finds the parent gameObject of the VRCStation 
+                        MelonLogger.Log("Destroyed chair object");
+                    }
+                }));
+        }
+
+    }
+}
